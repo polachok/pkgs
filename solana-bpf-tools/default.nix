71f2167499c07c5fc0c7735173044c9e41cab911
Alexander Polakov <a.polakov@zubr.io>
2022-01-12 23:25:42 +0300

    fix solana-bpf-tools

--- solana-bpf-tools/default.nix
+++ solana-bpf-tools/default.nix
@@ -16,36 +16,95 @@ let
     fetchSubmodules = true;
   };
   cargo_repo = fetchFromGitHub {
-    owner = "solana-labs";
+    owner = "rust-lang";
     repo = "cargo";
     rev = "rust-1.56.0";
-    sha256 = lib.fakeSha256;
+    sha256 = "0m8nac1apal4vcga48anh260r7xc9d21m0fcim2aavq3dygk9nr6";
   };
   newlib_repo = fetchFromGitHub {
     owner = "solana-labs";
     repo = "newlib";
     rev = "bpf-tools-v1.19";
-    sha256 = lib.fakeSha256;
+    sha256 = "03fvryw1cr9aglkbx0wwpilf99991l55lsqpyccy5ichjv6nb8l0";
   };
-  rustc_pkg = import ./rustc.nix { inherit rustPlatform lib fetchFromGitHub fetchgit fetchurl pkgs; };
-in
-pkgs.stdenv.mkDerivation {
-  pname = "solana-bpf-tools";
-  version = "1.19";
-
-  src = fetchFromGitHub {
+  bpf_tools_repo = fetchFromGitHub {
     owner = "solana-labs";
     repo = "bpf-tools";
     rev = "v1.19";
     sha256 = "0ad6b2k5shg6v1qww82jdfimr2pn9mzgh9pb8yk9i52f3q098qy9";
   };
+  #rustc_pkg = import ./rustc.nix { inherit rustPlatform lib fetchFromGitHub fetchgit fetchurl pkgs; };
+in
+pkgs.stdenv.mkDerivation rec {
+  pname = "solana-bpf-tools";
+  version = "1.19";
 
-  nativeBuildInputs = with pkgs; [ bash git python3 curl rustc cargo ];
+  src = [ bpf_tools_repo rust_repo cargo_repo newlib_repo ];
+  sourceRoot = ".";
+  #cargoRoot = "./bpf-tools/out/rust";
+
+  nativeBuildInputs = with pkgs; [
+    git python3
+    clang
+    cmake
+    ninja
+    rustPlatform.rust.rustc rustPlatform.rust.cargo
+    rustPlatform.cargoSetupHook
+  ];
   buildInputs = with pkgs; [ ];
+  outputs = ["out"];
+
+  patches = [ ./disable-git-clone.patch ./enable-vendor.patch ./disable-vendor-check.patch ];
+
+  dontConfigure = true;
+
+  cargoRoot = "./out/rust";
+  cargoVendorDir = "./out/rust/vendor";
+  cargoDeps = rustPlatform.importCargoLock {
+      lockFile = "${rust_repo}/Cargo.lock";
+      outputHashes = {
+        "compiler_builtins-0.1.52" = "0mwbxvr8x332irmgaqxxn3ij2wpdvpvaxc5qkw5fhq1rl7qbaivw";
+      };
+  };
+
+  postPatch = ''
+    patchShebangs ./out/rust/x.py
+    patchShebangs ./out/rust/build.sh
+    patchShebangs ./out/rust/src/etc
+
+    substituteInPlace ./build.sh --subst-var-by cargo ${rustPlatform.rust.cargo}/bin/cargo
+    substituteInPlace ./out/rust/config.toml --subst-var-by cargo ${rustPlatform.rust.cargo}/bin/cargo \
+      --subst-var-by rustc ${rustPlatform.rust.rustc}/bin/rustc
+  '';
+
+  unpackPhase = ''
+    echo "copying sources"
+    cp -r ${bpf_tools_repo} bpf-tools
+    chmod -R +w ./bpf-tools/
+    mkdir -p bpf-tools/out
+    cd ./bpf-tools
+    cp -r ${rust_repo} ./out/rust
+    cp -r ${cargo_repo} ./out/cargo
+    cp -r ${newlib_repo} ./out/newlib
+    echo "copying vendored sources"
+    chmod -R +w .
+    cp -r ${cargoDeps} ./out/rust/vendor
+    chmod -R +w ./out/rust
+    cp -r ./out/rust/vendor/.cargo ./out/rust/.cargo
+    sed -i 's/cargo-vendor-dir/vendor/' ./out/rust/.cargo/config 
+    chmod -R +w .
+    echo "copied"
+  '';
 
   buildPhase = ''
-     echo ${rustc_pkg}
-    #./x.py build --stage 1 --target=x86_64-unknown-linux-gnu,bpfel-unknown-unknown --set=build.rustc=${pkgs.rustc}/bin/rustc --set=build.cargo=${pkgs.cargo}/bin/cargo
+    echo "== starting build"
+    export GITHUB_WORKSPACE=$(pwd)
+    bash ./build.sh
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r out/deploy/* $out/
   '';
 
   doCheck = false;
